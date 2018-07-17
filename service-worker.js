bd5b308ce95b1b4454f30653683abd5d25165157
turquoise-turtle <a3j2p1@gmail.com>
2018-07-17 18:26:10 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -26,10 +26,11 @@ self.addEventListener('fetch', function(evt) {
 function precache() {
   return caches.open(CACHE).then(function (cache) {
     return cache.addAll([
-      'index.html',
-      'alt.js',
-      'jszip.js',
-      'filesaver.js'
+      '/',
+      '/index.html',
+      '/alt.js',
+      '/jszip.js',
+      '/filesaver.js'
     ]);
   });
 }
